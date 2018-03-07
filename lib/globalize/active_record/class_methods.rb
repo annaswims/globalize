2c725e714b886f62e7311c91bf28fa6f1679c4bb
Anna Carey <19188+annaswims@users.noreply.github.com>
2018-03-07 15:08:52 -0500

    Revert "Resolve deprecation warning: attribute is not an attribute known to Active Record."
    This reverts commit e946e63983d42c9ff49b39b13ffcc3ad45626dd3.

--- lib/globalize/active_record/class_methods.rb
+++ lib/globalize/active_record/class_methods.rb
@@ -98,7 +98,6 @@ module Globalize
       end
 
       def define_translated_attr_accessor(name)
-        attribute(name)
         define_translated_attr_reader(name)
         define_translated_attr_writer(name)
       end
